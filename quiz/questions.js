7d4bd2fe6fb7a8e00e111bfdd8a5048ee9cff869
Tryingtolearnunity <104686087+Tryingtolearnunity@users.noreply.github.com>
2022-06-10 19:20:45 -0300

    Create questions.js

---
+++ quiz/questions.js
@@ -0,0 +1,72 @@
+//creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "QUE ES UN DESIERTO POLAR?",
+    answer: "Un Bioma Helado",
+    options: [
+      "Un Bioma Templado",
+      "Un Bioma Helado",
+      "Un Bioma Cálido",
+      "Un Bioma Húmedo"
+    ]
+  },
+    {
+    numb: 2,
+    question: "CUALES SON SUS ANIMALES?",
+    answer: "Osos Polares, Pinguinos, Etc.",
+    options: [
+      "Perros, Gatos, Etc.",
+      "Peces, Tiburones, Etc.",
+      "Elefantes, Leones, Etc.",
+      "Osos Polares, Pinguinos, Etc."
+    ]
+  },
+    {
+    numb: 3,
+    question: "DONDE ES SU UBICACION?",
+    answer: "Antártida, Alaska, Etc.",
+    options: [
+      "Antártida, Alaska, Etc.",
+      "Sudáfrica, Etiopía, Etc.",
+      "Alemania, Italia, Etc.",
+      "Australia, Nueva Zelanda, Etc."
+    ]
+  },
+    {
+    numb: 4,
+    question: "CUANTAS PRECIPITACIONES ANUALES?",
+    answer: "250 mm",
+    options: [
+      "1000 mm",
+      "20 mm",
+      "0 mm",
+      "250 mm"
+    ]
+  },
+    {
+    numb: 5,
+    question: " CUALES SON SUS PLANTAS?",
+    answer: "Algas Terrestres, Musgos, Etc.",
+    options: [
+      "Algas Terrestres, Musgos, Etc.",
+      "Palmeras, Cocos, Etc.",
+      "Enredaderas, Trepadoras, Etc.",
+      "Rosas, Margaritas, Etc."
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
